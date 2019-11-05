93013a5739da7e9f035425fdf4891891f0402556
adams_zhou <19533391@qq.com>
2019-11-05 22:42:55 +0800

    test

--- temp/smd/src/main/java/com/adams/smd/mapper/WeatherInfoMapper.java
+++
@@ -1,9 +0,0 @@
-package com.adams.smd.mapper;
-
-import java.util.List;
-
-import com.adams.smd.domain.WeatherInfo;
-
-public interface WeatherInfoMapper {
-	List<WeatherInfo> getAllWeatherInfo();
-}
